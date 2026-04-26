d9afb8e52a082da28c4770d72922ae41a8d1cad9
Arcrein <arcrein@outlook.com>
2026-04-25 22:34:26 -0600

    changes

--- Core/BattleScreen.py
+++ Core/BattleScreen.py
@@ -24,8 +24,7 @@ class BattleScreen:
         self.playerName = name
 
     def startCombant(self):
-        print(self.playerName)
-        print(self.selectedCharacter)
+        self.buttonBar=tk.PhotoImage(file="assets/img/buttonBar.png")
         self.playerPokemons=[]
         for p in self.selectedPokes.selectedPokemons:
             self.playerPokemons.append(Pk.Pokemon.newPokemon(self.selectedPokes.pokeList[p],self.movementList))
@@ -35,5 +34,33 @@ class BattleScreen:
         for i in range(0,3):
             pok=self.selectedPokes.pokeList[random.randint(0,len(self.selectedPokes.pokeList)-1)]
             self.enemyPokemons.append(Pk.Pokemon.newPokemon(pok,self.movementList))
-        print(self.playerPokemons[0].Especie.name)
-        print(self.enemyPokemons[0].Especie.name)
+        self.setUpChar()
+        self.frm.create_image(512/2, 342, image=self.buttonBar, anchor="center")
+        self.enemyIntro=True
+    
+    def moveChar(self):
+        #mueve enemigo    
+        x,y=self.frm.coords(self.enemyImgPlacement)
+        if x > self.targetEnemyImg:
+            dx=(self.targetEnemyImg-x)*0.1
+            self.frm.move(self.enemyImgPlacement,dx,0)
+            if abs(dx)<0.2:
+                self.enemyIntro=False
+
+        if self.enemyIntro==False:
+            #mueve jugador
+            x,y=self.frm.coords(self.playerImgPlacement)
+            if x < self.targetPlayerImg:
+                dx=(self.targetPlayerImg-x)*0.1
+                self.frm.move(self.playerImgPlacement,dx,0)
+
+        self.frm.after(33,self.moveChar)
+
+    def setUpChar(self):
+        self.playerCharimg = tk.PhotoImage(file="assets/trainers/"+self.selectedCharacter+"Back.png")
+        self.enemyCharimg = tk.PhotoImage(file="assets/trainers/"+self.selectedEnemyChar+".png")
+        self.targetPlayerImg = 88
+        self.targetEnemyImg = 353
+        self.playerImgPlacement = self.frm.create_image(-230, 100, image=self.playerCharimg, anchor="nw")
+        self.frm.after(33,self.moveChar)
+        self.enemyImgPlacement = self.frm.create_image(513, 32, image=self.enemyCharimg, anchor="nw")
